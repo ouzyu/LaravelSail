0f2a52ec3c607842d9ffc3ce92de53f435efe47f
ouzyu <murahiro927@gmail.com>
2023-10-01 11:39:51 +0900

    [ログイン・ログアウト機能]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,13 @@
     </head>
 
     <body>
-        <p>Hello World</p>
+        <p>こんにちは！
+        @if (Auth::check())
+            {{ \Auth::user()->name }}さん</p>
+            <p><a href="/logout">ログアウト</a></p>
+        @else
+            ゲストさん</p>
+            <p><a href="/login">ログイン</a><br><a href="/register">会員登録</a></p>
+        @endif
     </body>
 </html>
